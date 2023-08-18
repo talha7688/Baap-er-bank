020d5e3c326006fe06fba8f8f61de00142511423
unknown <talha7688>
2023-08-18 23:50:08 +0600

    added baap er bank html file

--- login.js
+++ login.js
@@ -1,4 +1,14 @@
 // step-1
 document.getElementById('btn-submit').addEventListener('click', function(){
+  const emailField = document.getElementById('user-email');
+  const email= emailField.value;
   
+
+  const passField = document.getElementById('user-pass');
+  const password = passField.value;
+  if(email == 'sayedabutalha00@gmail.com' && password == '998800'){
+    window.location.href = 'bank.html'
+  }else{
+    alert('bhag bsdk')
+  }
 })
