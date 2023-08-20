57addbc3211ef22dfe5ec0118af6dbdfcf71fd64
unknown <talha7688>
2023-08-20 14:10:46 +0600

    project fully completed

--- withdraw.js
+++ withdraw.js
@@ -7,5 +7,12 @@ document.getElementById('btn-withdraw').addEventListener('click', function(){
    const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
     const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
     withdrawTotalElement.innerText = currentWithdrawTotal;
+
+
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+    balanceTotalElement.innerText = newBalanceTotal;
     withdrawField.value = "";
 })
