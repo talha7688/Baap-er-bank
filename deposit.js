8b36665aa74217ed0b7c0c8821e87c245ef65ca3
unknown <talha7688>
2023-08-20 13:21:47 +0600

    btn deposit work

--- deposit.js
+++ deposit.js
@@ -1,4 +1,5 @@
 document.getElementById('btn-deposit').addEventListener('click', function(){
+    // deposit sector
     const depositField = document.getElementById('deposit-field');
     const newDepositAmountString = depositField.value;
     const newDepositAmount = parseFloat(newDepositAmountString);
@@ -7,11 +8,12 @@ document.getElementById('btn-deposit').addEventListener('click', function(){
     const previousDepositTotal = parseFloat(previousDepositTotalString);
     const currentDeposiTotal = previousDepositTotal + newDepositAmount;
     depositTotalElement.innerText = currentDeposiTotal;
-    
-    
-
-
-
+    // balance sector 
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+    const currentBalanceTotal = previousBalanceTotal + newDepositAmount;
+    balanceTotalElement.innerText = currentBalanceTotal;
     depositField.value = "";
 })
 
