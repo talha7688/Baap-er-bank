619a22eee8ce2afd13038f285080b4c8ddfb1045
unknown <talha7688>
2023-08-20 12:51:20 +0600

    added the deposit sector

---
+++ deposit.js
@@ -0,0 +1,17 @@
+document.getElementById('btn-deposit').addEventListener('click', function(){
+    const depositField = document.getElementById('deposit-field');
+    const newDepositAmountString = depositField.value;
+    const newDepositAmount = parseFloat(newDepositAmountString);
+    const depositTotalElement = document.getElementById('deposit-total');
+    const previousDepositTotalString = depositTotalElement.innerText;
+    const previousDepositTotal = parseFloat(previousDepositTotalString);
+    const currentDeposiTotal = previousDepositTotal + newDepositAmount;
+    depositTotalElement.innerText = currentDeposiTotal;
+    
+    
+
+
+
+    depositField.value = "";
+})
+
